5893bced961255f69235ce231b82e8f3f3d3b1ac
mishaprysiazhniuk <mishaprisyajnyuk@knu.ua>
2022-11-08 11:39:31 +0200

    first modification

---
+++ Fibonachi.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+//Fibonachi
+int fibonacci(int number)
+{
+    if (number == 0)
+        return 0; 
+    if (number == 1)
+        return 1; 
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+
+int main()
+{
+    for (int count = 0; count < 13; ++count)
+        std::cout << fibonacci(count) << " ";
+
+    return 0;
+}
